aef8f51818886222840a8f8a503a1a3727bc9855
mayank-0103 <201279422+mayank-0103@users.noreply.github.com>
2025-07-19 22:54:05 +0530

    FEATURE: Pagination added

--- controllers/postshortener.controller.js
+++ controllers/postshortener.controller.js
@@ -1,13 +1,23 @@
 import crypto from 'crypto';
-import {deleteLinks, Devs, editShortLinkDatabase, loadLinks, saveLinks} from '../models/shortener.model.js';
+import { deleteLinks, Devs, editShortLinkDatabase, loadLinks, saveLinks } from '../models/shortener.model.js';
 import path from 'path';
-import {shortCodeSchema} from "../validators/shortcode-validator.js";
+import { shortCodeSchema, shortenerSearchParamsSchema } from "../validators/shortcode-validator.js";
 import z from "zod";
 
 export const getShortenerPage = async (req, res) => {
     if (!req.user) return res.redirect("/login");
     try {
-        const links = await loadLinks(req.user.id);
+        const { data: searchParams, error } = shortenerSearchParamsSchema.safeParse(req.query);
+
+        // const links = await loadLinks(req.user.id);
+        const { shortLinks, totalCount } = await loadLinks({
+            userId: req.user.id,
+            limit: 10,
+            offset: (searchParams.page - 1) * 10
+        });
+
+        //! totalCount = 100
+        const totalPages = Math.ceil(totalCount / 10);
 
         //* Getting data of cookies (Using Node.js)
         // let cookieData = req.headers.cookie;
@@ -27,7 +37,7 @@ export const getShortenerPage = async (req, res) => {
         //? console.log('Status(Logged IN) : ', isLoggedIn);
         //? console.log('Status(Is HUMAN) : ', userName);
 
-        res.render("index", {links: links, host: req.host, errors: req.flash("errors")});
+        res.render("index", { links: shortLinks, host: req.host, currentPage: searchParams.page, totalPages: totalPages, errors: req.flash("errors") });
     } catch (error) {
         console.error(error);
         return res.status(500).sendFile(path.join(import.meta.dirname, '..', 'views', 'server_error.html'));
@@ -38,19 +48,20 @@ export const postURLShortener = async (req, res) => {
     if (!req.user) return res.redirect("/login");
     try {
         //* Getting links from MySQL database
-        const {url, shortCode} = req.body;
+        const { url, shortCode } = req.body;
         const finalShortCode = shortCode || crypto.randomBytes(4).toString('hex');
 
         //! Using Zod Validation
-        const {error} = shortCodeSchema.safeParse({url, shortCode : finalShortCode});
+        const { error } = shortCodeSchema.safeParse({ url, shortCode: finalShortCode });
 
         if (error) {
-            const errors = error.errors.reduce((acc, e, i) => acc + `${i+1}) ` + e.message + "<br>", '');
+            const errors = error.errors.reduce((acc, e, i) => acc + `${i + 1}) ` + e.message + "<br>", '');
             req.flash("errors", errors);
             return res.redirect("/");
         }
 
-        const [links] = await loadLinks(req.user.id, finalShortCode);
+        const { shortLinks } = await loadLinks({ userId: req.user.id, shortCode: finalShortCode });
+        const links = shortLinks?.[0] || null;
         if (links) {
             req.flash("errors", "Short Code already exists. Please choose another.");
             return res.redirect("/");
@@ -71,9 +82,11 @@ export const postURLShortener = async (req, res) => {
 
 export const redirectToShortLink = async (req, res) => {
     try {
-        const {shortCode} = req.params;
+        const { shortCode } = req.params;
+        console.log(req.params);
         if (!req.user) return res.status(404).sendFile(path.join(import.meta.dirname, '..', 'views', '404.html'));
-        const [links] = await loadLinks(req.user.id, shortCode);
+        const { shortLinks } = await loadLinks({ userId: req.user.id, shortCode: shortCode });
+        const links = shortLinks?.[0] || null;
         if (!links) return res.status(404).sendFile(path.join(import.meta.dirname, '..', 'views', '404.html'));
         console.log(`Redirected to : ${links.shortCode}`);
         return res.redirect(302, links.url);
@@ -84,7 +97,7 @@ export const redirectToShortLink = async (req, res) => {
 };
 
 export const getEJSDeveloperPage = (req, res) => {
-    res.render("devpage", {Devs});
+    res.render("devpage", { Devs });
 };
 
 export const deleteShortCode = async (req, res) => {
@@ -92,10 +105,10 @@ export const deleteShortCode = async (req, res) => {
     const shortCode = req.params.id;
     const resFromDB = await deleteLinks(shortCode, req.user.id);
     if (resFromDB) {
-        const response = {success: true, message: `Deleted Short Code: ${shortCode}`};
+        const response = { success: true, message: `Deleted Short Code: ${shortCode}` };
         res.status(200).json(response);
     } else {
-        const response = {success: false, message: `Failed to delete short code: ${shortCode}`};
+        const response = { success: false, message: `Failed to delete short code: ${shortCode}` };
         res.status(401).json(response);
     }
 };
@@ -107,19 +120,19 @@ export const editShortLink = async (req, res) => {
     const { data: id, error } = z.coerce.number().int().safeParse(req.params.id);
     if (error) return res.status(404).sendFile(path.join(import.meta.dirname, '..', 'views', '404.html'));
 
-    const {url, shortCode} = req.body;
+    const { url, shortCode } = req.body;
 
     //! Using Zod Validation
     const { error: validationError } = shortCodeSchema.safeParse(req.body);
 
     if (validationError) {
-        const errors = validationError.errors.reduce((acc, e, i) => acc + `${i+1}) ` + e.message + "<br>", '<br>');
-        return res.status(409).json({success: false, redirectTo: "/", error: errors});
+        const errors = validationError.errors.reduce((acc, e, i) => acc + `${i + 1}) ` + e.message + "<br>", '<br>');
+        return res.status(409).json({ success: false, redirectTo: "/", error: errors });
     }
     try {
         const resp = await editShortLinkDatabase(id, url, shortCode, req.user.id);
-        if (!resp[0].affectedRows) return res.status(404).json({success: false, redirectTo: "/", error: "Can't update short code<br>May be deleted"});
-        return res.status(200).json({success: true, redirectTo: "/"});
+        if (!resp[0].affectedRows) return res.status(404).json({ success: false, redirectTo: "/", error: "Can't update short code<br>May be deleted" });
+        return res.status(200).json({ success: true, redirectTo: "/" });
     } catch (err) {
         console.error(err);
         return res.status(500).sendFile(path.join(import.meta.dirname, '..', 'views', 'server_error.html'));
--- models/shortener.model.js
+++ models/shortener.model.js
@@ -1,10 +1,11 @@
-import {and, eq} from "drizzle-orm";
-import {db} from "../config/db.js";
-import {short_link} from "../drizzle/schema.js";
-import {logoutUser} from "../controllers/auth.controller.js";
+import { and, count, desc, eq } from "drizzle-orm";
+import { db } from "../config/db.js";
+import { short_link } from "../drizzle/schema.js";
+import { logoutUser } from "../controllers/auth.controller.js";
 
-export const loadLinks = async (userId, shortCode = null) => {
+export const loadLinks = async ({ userId, shortCode = null, limit = 10, offset = 0 }) => {
     let res;
+    let totalCount;
 
     try {
         if (shortCode) {
@@ -14,13 +15,17 @@ export const loadLinks = async (userId, shortCode = null) => {
                     eq(short_link.userId, userId)
                 )
             );
+            totalCount = 1;
         } else {
-            res = await db.select().from(short_link).where(eq(short_link.userId, userId));
+            const condition = eq(short_link.userId, userId);
+            res = await db.select().from(short_link).where(condition).orderBy(desc(short_link.createdAt)).limit(limit).offset(offset);
+
+            [{ totalCount }] = await db.select({ totalCount: count() }).from(short_link).where(condition);
         }
     } catch {
         await logoutUser();
     }
-    return res?.length ? res : [];
+    return res?.length ? { shortLinks: res, totalCount } : { shortLinks: [], totalCount: 0 };
 };
 
 export const saveLinks = async (shortCode, url, userId) => {
@@ -59,11 +64,11 @@ export const Devs = [
 
 export const editShortLinkDatabase = async (id, url, shortCode, userId) => {
     return (db.update(short_link)
-        .set({url: url, shortCode: shortCode})
+        .set({ url: url, shortCode: shortCode })
         .where(and(
-                eq(short_link.id, id),
-                eq(short_link.userId, userId)
-            )
+            eq(short_link.id, id),
+            eq(short_link.userId, userId)
+        )
         )
     )
 };
